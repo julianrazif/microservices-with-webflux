ac0262d5cc8fed6708b9eb924a00585057d66d9a
julian razif figaro <julian.razif@gmail.com>
2022-09-08 19:01:43 +0700

    add simple math service

---
+++ simple-math-service/src/main/java/com/example/microservices/service/simple/math/SimpleMathServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices.service.simple.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMathServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleMathServiceApplication.class, args);
+  }
+}
